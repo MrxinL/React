a2b03180957a7d7c1df858c4de5cb546e884013d
LVXIN <1392846225@qq.com>
2023-10-30 16:02:30 +0800

    登录

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,70 @@
 
-function login() {
-    return(
+import {useState} from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+import './index.css';
+import { Get } from '../../utils/index'
+function Login() {
+    const [name, setName] = useState(void 0)
+    const [password, setPassword] = useState(void 0)
+    const handleGetName = (e: any) => {
+        setName(e.target.value)
+    }
+
+    const handleGetPassword = (e: any) => {
+        setPassword(e.target.value)
+    }
+    const handleSubmit = async () => {
+       const data: any = await Get('https://mock.mengxuegu.com/mock/653f413882cf792009576e18/example/mock/login')
+       const { data: { res} } = data
+       if(res.name === name && res.password === password) {
+           console.log(true)
+       } else {
+           console.log(false)
+       }
+    }
+    return (
         <div className="login-index">
-            
+            <div className='login-input'>
+                <Form>
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input 
+                        placeholder="账号" 
+                        onChange={handleGetName}
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password
+                        placeholder="密码：123456"
+                        onChange={handleGetPassword}
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{ offset: 8, span: 16 }}
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
     )
 }
 
-export default login
+export default Login
---
+++ src/utils/index.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const Get = (parmes: string) => {
+    return new Promise((res, rej) => {
+    axios.get(parmes).then((respoone: any) => {
+        res(respoone)
+    }).catch(e => {
+        rej(e)
+    })
+
+    })
+}
+
+const Post = (parmes: string, data: object) => {
+    axios.post(parmes, data)
+}
+export {Get, Post}
